14c2a7d1dacda459d2e74c8d63cb59aa112143ce
Denys Karpenko <denisdataforsep@gmail.com>
2025-07-13 19:19:19 -0700

    add image validation and fix test

--- internal/composebuilder/domain/valueobjects/image.go
+++ internal/composebuilder/domain/valueobjects/image.go
@@ -1,17 +1,24 @@
 package valueobjects
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Image struct {
 	imageName string
 	tag       string
 }
 
-func NewImage(imageName string, tag string) Image {
+func NewImage(imageName string, tag string) (Image, error) {
+	if imageName == "" || tag == "" {
+		return Image{}, errors.New("image or can't be empty")
+	}
+
 	return Image{
 		imageName: imageName,
 		tag:       tag,
-	}
+	}, nil
 }
 
 func (i Image) Tag() string {
--- internal/composebuilder/domain/valueobjects/image_test.go
+++ internal/composebuilder/domain/valueobjects/image_test.go
@@ -8,18 +8,57 @@ func TestNewImage(t *testing.T) {
 		imageName string
 		tag       string
 		expected  Image
+		expectErr bool
+		errorMsg  string
 	}{
 		{
 			name:      "creates image with valid name and tag",
 			imageName: "nginx",
 			tag:       "1.21.0",
 			expected:  Image{imageName: "nginx", tag: "1.21.0"},
+			expectErr: false,
+		},
+		{
+			name:      "creates image with valid name and tag",
+			imageName: "nginx",
+			tag:       "latest",
+			expected:  Image{imageName: "nginx", tag: "latest"},
+			expectErr: false,
+		},
+		{
+			name:      "creates image with empty tag",
+			imageName: "nginx",
+			tag:       "",
+			expected:  Image{},
+			expectErr: true,
+			errorMsg:  "image or can't be empty",
+		},
+		{
+			name:      "creates with empty image",
+			imageName: "",
+			tag:       "latest",
+			expected:  Image{},
+			expectErr: true,
+			errorMsg:  "image or can't be empty",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			result := NewImage(test.imageName, test.tag)
+			result, err := NewImage(test.imageName, test.tag)
+
+			if test.expectErr {
+				if err == nil {
+					t.Error("NewImage() expected error but got none")
+					return
+				}
+
+				if err.Error() != test.errorMsg {
+					t.Errorf("NewImage() expected error message %s but got %s", test.errorMsg, err.Error())
+					return
+				}
+			}
+
 			if result != test.expected {
 				t.Errorf("NewImage() = %v, want %v", result, test.expected)
 			}
