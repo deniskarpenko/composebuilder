640aae646cca262b8f362b94d654cecbf29317e8
Denys Karpenko <denisdataforsep@gmail.com>
2025-08-04 19:20:47 -0700

    keep working with builders

--- internal/composebuilder/domain/entities/docker_compose_container.go
+++ internal/composebuilder/domain/entities/docker_compose_container.go
@@ -37,3 +37,47 @@ func NewContainerBuilder(containerName string, logger *slog.Logger) *ContainerBu
 		},
 	}
 }
+
+func (cb *ContainerBuilder) WithImage(image *valueobjects.Image) *ContainerBuilder {
+	cb.container.image = image
+	return cb
+}
+
+func (cb *ContainerBuilder) WithBuild(build *valueobjects.Build) *ContainerBuilder {
+	cb.container.build = build
+	return cb
+}
+
+func (cb *ContainerBuilder) WithPorts(ports ...valueobjects.Ports) *ContainerBuilder {
+	cb.container.ports = ports
+	return cb
+}
+
+func (cb *ContainerBuilder) WithVolumes(volumes ...valueobjects.Volume) *ContainerBuilder {
+	cb.container.volumes = volumes
+	return cb
+}
+
+func (cb *ContainerBuilder) WithEnvs(envs ...valueobjects.Env) *ContainerBuilder {
+	cb.container.envs = envs
+	return cb
+}
+
+func (cb *ContainerBuilder) WithEnvFiles(envFiles ...string) *ContainerBuilder {
+	cb.container.envFiles = envFiles
+	return cb
+}
+
+func (cb *ContainerBuilder) WithNetworks(networks ...string) *ContainerBuilder {
+	cb.container.networks = networks
+	return cb
+}
+
+func (cb *ContainerBuilder) WithDependencies(dependencies ...string) *ContainerBuilder {
+	cb.container.dependsOn = dependencies
+	return cb
+}
+
+func (cb *ContainerBuilder) Build() Container {
+	return cb.container
+}
