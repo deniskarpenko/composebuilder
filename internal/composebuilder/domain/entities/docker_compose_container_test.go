15088fd04f0789f5ce6ec690fc22369dc4971fa9
Denys Karpenko <denisdataforsep@gmail.com>
2025-08-04 19:28:46 -0700

    keep working with builders

---
+++ internal/composebuilder/domain/entities/docker_compose_container_test.go
@@ -0,0 +1,19 @@
+package entities
+
+import (
+	"log/slog"
+	"os"
+	"testing"
+)
+
+func TestNewContainerBuilder(t *testing.T) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	containerName := "test container"
+
+	builder := NewContainerBuilder(containerName, logger)
+
+	if builder == nil {
+		t.Fatal("Expected builder to be non-nil")m
+	}
+
+}
