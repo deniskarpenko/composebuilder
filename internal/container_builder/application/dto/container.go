162631557f0959606e811cf6f0f4ae6143dbf1d6
Denys Karpenko <denisdataforsep@gmail.com>
2025-09-01 18:50:42 -0700

    start application layer

---
+++ internal/container_builder/application/dto/container.go
@@ -0,0 +1,81 @@
+package dto
+
+type Port struct {
+	hostPort      string
+	containerPort uint
+}
+
+func NewPort(hostPort string, containerPort uint) Port {
+	return Port{hostPort: hostPort, containerPort: containerPort}
+}
+
+type Volume struct {
+	hostPath      string
+	containerPath string
+}
+
+func NewVolume(hostPath string, containerPath string) Volume {
+	return Volume{hostPath: hostPath, containerPath: containerPath}
+}
+
+type Image struct {
+	imageName string
+	tag       string
+}
+
+func NewImage(imageName string, tag string) Image {
+	return Image{imageName: imageName, tag: tag}
+}
+
+type Build struct {
+	dockerfilePath string
+	contextPath    string
+}
+
+func NewBuild(dockerfilePath string, contextPath string) Build {
+	return Build{dockerfilePath: dockerfilePath, contextPath: contextPath}
+}
+
+type Env struct {
+	variable string
+	value    string
+}
+
+func NewEnv(variable string, value string) Env {
+	return Env{variable: variable, value: value}
+}
+
+type Container struct {
+	name      string
+	image     *Image
+	build     *Build
+	ports     []Port
+	volumes   []Volume
+	envs      []Env
+	envFiles  []string
+	networks  []string
+	dependsOn []string
+}
+
+func NewContainer(
+	name string,
+	image *Image,
+	build *Build,
+	ports []Port,
+	volumes []Volume,
+	envs []Env,
+	envFiles []string,
+	networks []string,
+	dependsOn []string,
+) Container {
+	return Container{
+		name:      name,
+		image:     image,
+		build:     build,
+		ports:     ports,
+		volumes:   volumes,
+		envs:      envs,
+		envFiles:  envFiles,
+		dependsOn: dependsOn,
+	}
+}
--- internal/container_builder/application/services/container_application_service_test.go
+++ internal/container_builder/application/services/container_application_service_test.go
@@ -1,6 +1,26 @@
 package services
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/deniskarpenko/composebuilder/internal/container_builder/application/dto"
+)
 
 func TestContainerApplicationService_CreateApplication(t *testing.T) {
+}
+
+func createPhpContainer() dto.Container {
+	phpBuild := dto.NewBuild("php.Dockerfile", "./dockerfiles")
+
+	return dto.NewContainer(
+		"php",
+		nil,
+		&phpBuild,
+		nil,
+		nil,
+		nil,
+		nil,
+		nil,
+		nil,
+	)
 }
