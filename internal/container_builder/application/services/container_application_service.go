14b84e01d5cb2d4191b1ea9ab0e9e51c22380797
Denys Karpenko <denisdataforsep@gmail.com>
2025-09-03 06:15:11 -0700

    developing application layer through TDD

--- internal/container_builder/application/services/container_application_service.go
+++ internal/container_builder/application/services/container_application_service.go
@@ -1,7 +1,9 @@
 package services
 
+import "github.com/deniskarpenko/composebuilder/internal/container_builder/application/dto"
+
 type ContainerApplicationService struct{}
 
-func (s *ContainerApplicationService) CreateApplication() {
+func (s *ContainerApplicationService) CreateApplication(project dto.Project) {
 
 }
